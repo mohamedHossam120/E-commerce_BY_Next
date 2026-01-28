a66b2e819e7eecb2354bac82858424d8c49230f0
mohamedHossam120 <mohamedhossameldeen12@gmail.com>
2026-01-28 04:41:50 +0200

    second commit

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -4,8 +4,9 @@ import { cookies } from "next/headers";
 
 export async function getUserToken(){
     
+    const TokenSession=(process.env.NODE_ENV==="production"?'__Secure-next-auth.session-token':'next-auth.session-token')
     const cookiesData=await cookies();
-    const encryptToken=cookiesData.get("next-auth.session-token")?.value;
+    const encryptToken=cookiesData.get(TokenSession)?.value;
 
     const data=await decode({token:encryptToken,secret:process.env.NEXTAUTH_SECRET!})
     console.log(data);
